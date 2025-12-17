8222bb9a55558815584c34ffd5e9c18b03182fd7
Ilya <ilyareztcov@gmail.com>
2025-12-17 22:20:43 +0300

    start menutabs

---
+++ ui/MenuTabsWidget.py
@@ -0,0 +1,10 @@
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QToolButton
+
+
+class MenuTabs(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
