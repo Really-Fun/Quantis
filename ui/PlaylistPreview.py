26a59cab86fcaf5a7c397c547577e48598d80519
Ilya <ilyareztcov@gmail.com>
2026-01-23 18:57:03 +0300

    add playlistpreview

--- ui/PlaylistPreview.py
+++ ui/PlaylistPreview.py
@@ -8,39 +8,42 @@ from PySide6.QtWidgets import (
 from PySide6.QtGui import QPixmap
 from PySide6.QtCore import Qt
 
+from providers import PathProvider
+
 
 class PlaylistPreview(QWidget):
     def __init__(self, playlist, parent=None):
         super().__init__(parent)
 
+        self.path_provider = PathProvider()
         self.playlist = playlist
         self.setObjectName("PlaylistPreview")
-        self.setFixedHeight(90)
+        self.setFixedHeight(160)
 
-        self.main_layout = QHBoxLayout(self)
+        self.main_layout = QVBoxLayout(self)
         self.main_layout.setContentsMargins(10, 10, 10, 10)
         self.main_layout.setSpacing(12)
 
         # ---------- COVER ----------
         self.cover = QLabel()
-        self.cover.setFixedSize(64, 64)
+        self.cover.setFixedSize(120,120)
         self.cover.setAlignment(Qt.AlignCenter)
         self.cover.setObjectName("Cover")
 
-        if playlist.cover_path:
-            pixmap = QPixmap(playlist.cover_path)
-            self.cover.setPixmap(
-                pixmap.scaled(
-                    64, 64,
-                    Qt.KeepAspectRatioByExpanding,
-                    Qt.SmoothTransformation
-                )
+        if not playlist.cover_path:
+            playlist.cover_path = self.path_provider.get_cover_path(playlist.tracks.values[0])
+
+        pixmap = QPixmap(playlist.cover_path)
+        self.cover.setPixmap(
+            pixmap.scaled(
+                120, 120,
+                Qt.KeepAspectRatioByExpanding,
+                Qt.SmoothTransformation
             )
-        else:
-            self.cover.setText("🎵")
+        )
 
         # ---------- TEXT ----------
-        self.text_layout = QVBoxLayout()
+        self.text_layout = QHBoxLayout()
         self.text_layout.setSpacing(4)
 
         self.title = QLabel(playlist.name)
