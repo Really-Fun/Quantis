516c4201e0b458ec6a47ba8a132c8bd4088a5040
ReallyFun <ilyareztcov@gmail.com>
2026-03-29 12:22:49 +0300

    delete and update: delete .bat and .spec, add windows adapter (test)

--- main.py
+++ main.py
@@ -7,27 +7,37 @@ from qasync import QEventLoop
 from PySide6.QtWidgets import QApplication
 from qt_material import apply_stylesheet
 
-from player import Player, NeonAppAdapter, NeonEventHandler
+from player import Player
 from services import TrackHistoryService
 from ui import NeonMusic
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    apply_stylesheet(app, "user_theme.xml", invert_secondary=True)
+    if os.path.exists("user_theme.xml"):
+        apply_stylesheet(app, "user_theme.xml", invert_secondary=False)
 
     loop = QEventLoop(app)
     asyncio.set_event_loop(loop)
 
     player = Player()
 
-    if platform.system() == "Linux":
+    current_os = platform.system()
+
+    if current_os == "Linux":
         from mpris_server.server import Server
+        from player import NeonAppAdapter, NeonEventHandler
         mpris_adapter = NeonAppAdapter(player)
         mpris = Server("NeonApp", mpris_adapter)
         event_handler = NeonEventHandler(mpris.root, mpris.player)
         mpris_adapter.set_event_handler(event_handler)
         mpris.publish()
+
+    elif current_os == "Windows":
+        from windows_adapter import WindowsSMTCAdapter 
+        windows_adapter = WindowsSMTCAdapter(player)
+        windows_adapter.publish()
+
     window = NeonMusic()
     window.show()
 
